3238442684fd80d06114ba7b645f54cc47ec4a11
vbaobao <vbao.main@gmail.com>
2021-09-11 22:45:39 -0700

    removed files

--- database/index.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true})
-.then(() => console.log('Connected to MongoDB.'))
-.catch(() => console.log('Error connecting.'));
