564da0b02571d865f50908e35ba3dceb017432cf
vbaobao <vbao.main@gmail.com>
2021-09-11 22:45:05 -0700

    update grom graphql to express

--- app.js
+++ app.js
@@ -1,74 +1,16 @@
-const bodyparser = require('body-parser');
+require('dotenv').config();
 const express = require('express');
-const { graphqlHTTP } = require('express-graphql');
-const { buildSchema } = require('graphql');
+const connection = require('./database/db');
+const users = require('./routes/users');
+const auth = require('./routes/auth');
 const app = express();
+const server = process.env.SERVER || 'localhost';
+const port = process.env.PORT || 3000;
 
-require('./database/index');
-const Event = require('./database/models/Event');
+app.use(express.json());
 
-app.use(bodyparser.json());
+app.use('/api/users', users);
+app.use('/api/auth', auth);
 
-app.use('/graphql', graphqlHTTP({
-  schema: buildSchema(`
-    type Event {
-      _id: ID!
-      title: String!
-      description: String!
-      price: Float!
-      date: String!
-    }
-
-    input EventInput {
-      title: String!
-      description: String!
-      price: Float!
-      date: String!
-    }
-
-    type RootQuery {
-      events: [Event!]!
-    }
-
-    type RootMutation {
-      createEvent(eventInput: EventInput!): Event
-    }
-
-    schema {
-      query: RootQuery,
-      mutation: RootMutation,
-    }
-  `),
-  rootValue: {
-    events: () => {
-      return Event.find()
-        .then(events => {
-          return events.map(event => {
-            return { ...event._doc, _id: event.id };
-          })
-        })
-        .catch(err => { throw err; });
-    },
-    createEvent: (args) => {
-      const event = new Event({
-        title: args.eventInput.title,
-        description: args.eventInput.description,
-        price: +args.eventInput.price,
-        date: new Date(args.eventInput.date),
-      });
-
-      return event
-        .save()
-        .then(result => {
-          return { ...result._doc };
-        })
-        .catch(err => {
-          console.error(err)
-          throw err;
-        });
-    },
-  },
-  graphiql: true
-}));
-
-app.listen(3000, () => console.log(`App is listening at http://localhost:3000`));
+connection();
+app.listen(3000, () => console.log(`App is listening at http://${server}:${port}`));
