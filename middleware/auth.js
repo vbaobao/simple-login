32734b03b39f6a9ab38ab8cd33ce9a228bfaa629
vbaobao <vbao.main@gmail.com>
2021-09-12 17:08:58 -0700

    debugged "Bearer" in auth token

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,14 +1,15 @@
+require('dotenv').config();
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
   try {
-    const token = req.headers.authorization;
+    const token = req.headers.authorization.split(' ')[1];
     if (!token) return res.status(403).send('Access denied.');
-
     const decoded = jwt.verify(token, process.env.JWTPRIVATEKEY);
     req.user = decoded;
     next();
   } catch (err) {
+    console.log(err);
     res.status(400).send('Invalid token.');
   }
 };
